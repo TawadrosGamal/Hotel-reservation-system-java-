00c4d48f276d8de83bccaa85cecf3ab32843fe83
Tawadros Gamal <50532484+TawadrosGamal@users.noreply.github.com>
2026-05-02 03:33:45 +0300

    Refactored by Project Antigravity

--- SweetRoom.java
+++ SweetRoom.java
@@ -1,33 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hotel;
 
 /**
- *
- * @author tawadros
+ * Represents a suite room in the hotel, inheriting basic room properties.
+ * It calculates the total reservation price based on the price per night.
  */
-public class SweetRoom extends Rooms{
-   double pricepernight;
+public class SuiteRoom extends Rooms {
+    private double pricePerNight;
 
-    public SweetRoom( int roomnumber,double pricepernight) {
-        super(roomnumber);
-        this.pricepernight = pricepernight;
+    /**
+     * Constructs a new SuiteRoom.
+     *
+     * @param roomNumber    The unique number for the room.
+     * @param pricePerNight The cost for a single night's stay.
+     */
+    public SuiteRoom(int roomNumber, double pricePerNight) {
+        super(roomNumber);
+        this.pricePerNight = pricePerNight;
     }
-  
-   @Override
-   public double totalpriceofreservation(){
-      return pricepernight*daysofreservation;
-      }
-       public String tostring(){
-      if(isreserved==true)
-          return "the room number is "+roomnumber+"the days of reservation are "+daysofreservation+" the price per night is "+pricepernight
-           +  "the room is reserved";
-     else
-           return "the room number is "+roomnumber+"the days of reservation are "+daysofreservation+" the price per nigt is "+pricepernight
-         +" the room is not reserved";
-      
-  }
+
+    /**
+     * Calculates the total price for the reservation based on the number of days.
+     * Returns 0 if the number of reservation days is not positive.
+     *
+     * @return The total price of the reservation.
+     */
+    @Override
+    public double totalpriceofreservation() {
+        if (daysofreservation <= 0) {
+            return 0.0;
+        }
+        return pricePerNight * daysofreservation;
+    }
+
+    /**
+     * Returns a string representation of the suite room, including its number,
+     * reservation details, price, and reservation status.
+     *
+     * @return A formatted string describing the room.
+     */
+    @Override
+    public String toString() {
+        String status = isreserved ? "reserved" : "not reserved";
+        return String.format(
+                "the room number is %d, the days of reservation are %d, the price per night is %.2f, the room is %s",
+                roomnumber,
+                daysofreservation,
+                pricePerNight,
+                status
+        );
+    }
 }
